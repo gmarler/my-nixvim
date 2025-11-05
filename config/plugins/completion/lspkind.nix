e8634407bf9c0f43da9d0e924c5982dda8442656
Gordon Marler <gmarler@gmarler.com>
2025-11-04 23:46:39 -0500

    Update Flake to 20251104; fix various updated settings

--- config/plugins/completion/lspkind.nix
+++ config/plugins/completion/lspkind.nix
@@ -1,9 +1,9 @@
 {
   plugins.lspkind = {
     enable = true;
-    symbolMap = {
-    };
-    extraOptions = {
+    settings = {
+      symbol_map = {
+      };
       maxwidth = 50;
       ellipsis_char = "...";
     };
