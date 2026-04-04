4cfafeae20d0a043aba099c30c1754325c917c49
Gordon Marler <gmarler@gmarler.com>
2026-04-03 21:38:36 -0400

    feat(hop): Add hop plugin as non-default EasyMotion

---
+++ modules/nixvim/plugins/hop/default.nix
@@ -0,0 +1,59 @@
+{ config, lib, ... }:
+{
+  plugins = {
+    hop = {
+      # hop.nvim documentation
+      # See: https://github.com/smoka7/hop.nvim
+      enable = config.gmarlervim.editor.motion == "hop";
+    };
+  };
+
+  keymaps = lib.optionals (config.gmarlervim.editor.motion == "hop") [
+    {
+      key = "f";
+      action.__raw = ''
+        function()
+          require'hop'.hint_char1({
+            direction = require'hop.hint'.HintDirection.AFTER_CURSOR,
+          })
+        end
+      '';
+      options.remap = true;
+    }
+    {
+      key = "F";
+      action.__raw = ''
+        function()
+          require'hop'.hint_char1({
+            direction = require'hop.hint'.HintDirection.BEFORE_CURSOR,
+          })
+        end
+      '';
+      options.remap = true;
+    }
+    {
+      key = "t";
+      action.__raw = ''
+        function()
+          require'hop'.hint_char1({
+            direction = require'hop.hint'.HintDirection.AFTER_CURSOR,
+            hint_offset = -1
+          })
+        end
+      '';
+      options.remap = true;
+    }
+    {
+      key = "T";
+      action.__raw = ''
+        function()
+          require'hop'.hint_char1({
+            direction = require'hop.hint'.HintDirection.BEFORE_CURSOR,
+            hint_offset = 1
+          })
+        end
+      '';
+      options.remap = true;
+    }
+  ];
+}
