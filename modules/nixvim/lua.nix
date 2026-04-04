6723bd20350c4927fe8d74e834cee77d5234504f
Gordon Marler <gmarler@gmarler.com>
2026-04-04 18:05:01 -0400

    refactor(lua): expose shared web tooling helper globally

--- modules/nixvim/lua.nix
+++ modules/nixvim/lua.nix
@@ -1,4 +1,8 @@
 {
+  extraFiles = {
+    "lua/gmarlervim/web_tools.lua".source = ./lua/gmarlervim/web_tools.lua;
+  };
+
   # Just a small boolean function to convert a boolean to a string
   extraConfigLuaPre = ''
     function bool2str(bool) return bool and "on" or "off" end
