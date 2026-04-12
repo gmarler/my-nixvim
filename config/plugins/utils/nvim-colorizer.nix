c432039773fa6a2710358a7239a3935308380752
Gordon Marler <gmarler@gmarler.com>
2026-04-12 10:20:09 -0400

    refactor(settings): Transfer basic NeoVim settings from old Nixvim config

--- config/plugins/utils/nvim-colorizer.nix
+++
@@ -1,5 +0,0 @@
-{
-  plugins.colorizer = {
-    enable = true;
-  };
-}
--- modules/nixvim/options.nix
+++ modules/nixvim/options.nix
@@ -9,6 +9,8 @@ let
   hasNeovim012OrNewer = lib.versionAtLeast neovimVersion "0.12";
 in
 {
+  # ALWAYS use the clipboard for ALL operations (instead of interacting with
+  # the "+" and/or "*" registers explicitly)
   # Note: The basic clipboard setup below is overridden by vim.g.clipboard in globals
   # to add timeout wrappers that prevent wl-copy from freezing Neovim
   clipboard = {
@@ -48,10 +50,11 @@ in
     # UI & Appearance
     number = true;
     relativenumber = true;
+    # Highlight the location of the cursor with "crosshairs"
     cursorline = true;
-    cursorcolumn = false;
+    cursorcolumn = true;
     signcolumn = "yes";
-    colorcolumn = "100";
+    colorcolumn = "100"; # Columns to highlight
     laststatus = 3; # Global statusline
     showtabline = 2;
     showmode = false;
@@ -66,7 +69,9 @@ in
     splitkeep = "screen";
 
     # Mouse
+    # Enable mouse support for 'a'll previous modes
     mouse = "a";
+    # Mouse right-click extends the current selection
     mousemodel = "extend"; # Right-click extends selection
 
     # Search
@@ -80,6 +85,7 @@ in
     undofile = true;
     autoread = true;
     writebackup = false;
+    # File-content encoding for the current buffer
     fileencoding = "utf-8";
     modeline = true; # Scan for editor directives like 'vim: set ft=nix:'
     modelines = 100; # Scan first/last 100 lines for modelines
@@ -91,11 +97,20 @@ in
     # Indentation & Formatting
     tabstop = 2;
     shiftwidth = 2;
+    # Expand TABs to SPACEs
     expandtab = true;
     autoindent = true;
     breakindent = true;
     copyindent = true;
     preserveindent = true;
+    # How autoformat should be done:
+    # r: ????
+    # q: Allow formatting of comments with "qq"
+    # n: When formatting text, recognize numbered lists
+    # l: Long lines are NOT broken in Insert mode - TBD: may need to revisit this
+    # 1: ???
+    # j: Where it makes sense, remove a comment leader when joining lines
+    # t: Auto-wrap text using "textwidth" (DISABLED)
     formatoptions = "rqnl1j";
     formatlistpat = "^\\s*[0-9\\-\\+\\*]\\+[\\.)]*\\s\\+";
     linebreak = true;
@@ -103,8 +118,8 @@ in
 
     # Folding
     foldlevel = 10; # Keep most folds open, but preserve structure
-    foldcolumn = "1";
-    foldenable = true;
+    foldcolumn = "1"; # Use 1 fixed column to show a fold
+    foldenable = true; # folds enabled by default
     foldmethod = "indent";
     foldnestmax = 10;
     foldlevelstart = -1; # -1 uses foldlevel value
@@ -131,6 +146,7 @@ in
     # Completion & Popup
     pumheight = 10; # Max popup menu items
     infercase = true;
+    # Options for Insert mode completion
     completeopt = [
       "menu"
       "menuone"
