f433177216bce188ed4fa8d6c1e385c5775907ea
Gordon Marler <gmarler@gmarler.com>
2026-04-29 00:37:42 -0400

    chore(flake): lock update

--- modules/nixvim/plugins/nix/default.nix
+++ modules/nixvim/plugins/nix/default.nix
@@ -1,9 +1,10 @@
+{ pkgs, ... }:
 {
   plugins = {
     # nixvim (nvim-nix) documentation
     # See: https://github.com/nix-community/nixvim
     direnv.enable = true;
-    nix.enable = true;
+    nix.enable = pkgs.stdenv.hostPlatform.isLinux;
     nix-develop.enable = true;
   };
 }
