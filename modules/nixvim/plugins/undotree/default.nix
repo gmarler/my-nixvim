1e07548e792d3a0d49a74678e23b8c15eb0f4ee4
Gordon Marler <gmarler@gmarler.com>
2026-06-25 10:54:37 -0400

    feat(utilities): support native undo tree
    Add gmarlervim.utilities.undoTree so bundled nvim.undotree can replace
    the external undotree plugin when a lean profile wants fewer managed
    plugins.
    
    Default and standard profiles keep the plugin provider.  minimal opts
    into native mode, disables the external plugin, and maps <leader>ueu to
    packadd nvim.undotree before running :Undotree

---
+++ modules/nixvim/plugins/undotree/default.nix
@@ -0,0 +1,53 @@
+{ config, lib, ... }:
+{
+  plugins = {
+    undotree = {
+      # undotree documentation
+      # See: https://github.com/mbbill/undotree
+      enable = config.gmarlervim.utilities.undoTree == "plugin";
+      # Put plugin in opt/ for lazy loading
+      autoLoad = false;
+
+      settings = {
+        CursorLine = true;
+        DiffAutoOpen = true;
+        DiffCommand = "diff";
+        DiffpanelHeight = 10;
+        HelpLine = true;
+        HighlightChangedText = true;
+        HighlightChangedWithSign = true;
+        HighlightSyntaxAdd = "DiffAdd";
+        HighlightSyntaxChange = "DiffChange";
+        HighlightSyntaxDel = "DiffDelete";
+        RelativeTimestamp = true;
+        SetFocusWhenToggle = true;
+        ShortIndicators = false;
+        TreeNodeShape = "*";
+        TreeReturnShape = "\\";
+        TreeSplitShape = "/";
+        TreeVertShape = "|";
+        WindowLayout = 2;
+      };
+    };
+  };
+
+  # Lazy load via lz-n
+  plugins.lz-n.plugins = lib.mkIf config.plugins.undotree.enable [
+    {
+      __unkeyed-1 = "undotree";
+      cmd = [
+        "UndotreeToggle"
+        "UndotreeShow"
+        "UndotreeHide"
+        "UndotreeFocus"
+      ];
+      keys = [
+        {
+          __unkeyed-1 = "<leader>ueu";
+          __unkeyed-2 = "<cmd>UndotreeToggle<CR>";
+          desc = "Undotree toggle";
+        }
+      ];
+    }
+  ];
+}
