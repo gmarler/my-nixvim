b9b0689100a00c664869525fbc2bc925a671efc8
Austin Horstman <khaneliman12@gmail.com>
2026-08-31 23:17:17 -0500

    refactor(kulala): use maintained built-in language server
    Replace archived kulala-ls with kulala.nvim's core-backed LSP and pin the matching core 0.37.0 release until Nixpkgs catches up.

--- modules/nixvim/lsp.nix
+++ modules/nixvim/lsp.nix
@@ -2,8 +2,6 @@
   config,
   lib,
   pkgs,
-  self,
-  system,
   ...
 }:
 {
@@ -114,13 +112,6 @@
       hyprls.enable = true;
       java_language_server.enable = config.gmarlervim.lsp.java == "java-language-server";
       jsonls.enable = true;
-      kulala_ls = {
-        enable = true;
-        config.cmd = [
-          (lib.getExe self.packages.${system}.kulala-ls)
-          "--stdio"
-        ];
-      };
       marksman.enable = true;
       nushell.enable = true;
       pyright.enable = config.gmarlervim.lsp.python.typeChecker == "pyright";
