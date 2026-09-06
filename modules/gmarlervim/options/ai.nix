khaneliman/khanelivim

refactor(integrations): scope the ai gate
The gate cleared every AI plugin, including providers that talk to a
local endpoint and need no account. Filter the merged list instead, so
minimal and offline profiles keep local completion.

Filtering happens in the option apply function. A mkForce definition
would read the same option it defines, which cycles.

modules/khanelivim/options/ai.nix
@@ -1,4 +1,11 @@
-{ lib, ... }:
+{ config, lib, ... }:
+let
+  gate = config.khanelivim.integrations.accountBacked;
+
+  # These plugins reach a local endpoint, so they need no account, token, or
+  # API key. The account-backed gate must keep them.
+  localPlugins = [ "minuet" ];
+in
 {
   options.khanelivim.ai = {
     plugins = lib.mkOption {
@@ -26,10 +33,19 @@
         "opencode"
         "sidekick"
       ];
+      # The account-backed gate filters the merged value here. It cannot force
+      # this option instead, because reading the list to filter it would create
+      # an evaluation cycle.
+      apply =
+        plugins: if gate.enable && gate.ai.enable then plugins else lib.intersectLists plugins localPlugins;
+
       description = ''
         List of AI plugins to enable.
         Multiple plugins can be enabled simultaneously.
         Set to [] to disable all AI features.
+
+        Disabling khanelivim.integrations.accountBacked.ai keeps only the
+        plugins that serve completions from a local endpoint.
 
         Available plugins:
         - avante: Claude AI interface with inline editing

modules/khanelivim/options/integrations.nix
@@ -30,10 +30,9 @@ in
 
   config = lib.mkMerge [
     (lib.mkIf (!cfg.enable || !cfg.ai.enable) {
-      khanelivim.ai = {
-        plugins = lib.mkForce [ ];
-        chatEnable = lib.mkForce false;
-      };
+      # khanelivim.ai.plugins drops its account-backed entries through the
+      # option's own apply function, which keeps local providers available.
+      khanelivim.ai.chatEnable = lib.mkForce false;
     })
     (lib.mkIf (!cfg.enable || !cfg.timeTracking.enable) {
       plugins.wakatime.enable = lib.mkForce false;

gmarler/my-nixvim

modules/gmarlervim/options/ai.nix
@@ -1,4 +1,11 @@
-{ lib, ... }:
+{ config, lib, ... }:
+let
+  gate = config.gmarlervim.integrations.accountBacked;
+
+  # These plugins reach a local endpoint, so they need no account, token, or
+  # API key. The account-backed gate must keep them.
+  localPlugins = [ "minuet" ];
+in
 {
   options.gmarlervim.ai = {
     plugins = lib.mkOption {
@@ -29,10 +36,19 @@
         "pairup"
         "sidekick"
       ];
+      # The account-backed gate filters the merged value here. It cannot force
+      # this option instead, because reading the list to filter it would create
+      # an evaluation cycle.
+      apply =
+        plugins: if gate.enable && gate.ai.enable then plugins else lib.intersectLists plugins localPlugins;
+
       description = ''
         List of AI plugins to enable.
         Multiple plugins can be enabled simultaneously.
         Set to [] to disable all AI features.
+
+        Disabling gmarlervim.integrations.accountBacked.ai keeps only the
+        plugins that serve completions from a local endpoint.
 
         Available plugins:
         - avante: Claude AI interface with inline editing

modules/gmarlervim/options/integrations.nix
@@ -30,10 +30,9 @@ in
 
   config = lib.mkMerge [
     (lib.mkIf (!cfg.enable || !cfg.ai.enable) {
-      gmarlervim.ai = {
-        plugins = lib.mkForce [ ];
-        chatEnable = lib.mkForce false;
-      };
+      # gmarlervim.ai.plugins drops its account-backed entries through the
+      # option's own apply function, which keeps local providers available.
+      gmarlervim.ai.chatEnable = lib.mkForce false;
     })
     (lib.mkIf (!cfg.enable || !cfg.timeTracking.enable) {
       plugins.wakatime.enable = lib.mkForce false;